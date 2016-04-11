a8f04e7b021f8cf09b97e5d0e42e543e56f816eb
Okan <okan.uzun@outlook.com>
2016-04-11 15:24:45 +0300

    işlem geçmişi görüntüleme

--- order-detail.php
+++ order-detail.php
@@ -1,6 +1,7 @@
-<?php include 'login-header.php'; ?>
+<?php include 'login-header.php';
+include 'dbsettings.php';
 
-        <div class="page-wrapper">
+    echo '<div class="page-wrapper">
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-xs-12">
@@ -11,7 +12,7 @@
                     <div class="col-xs-12">
                         <div class="panel panel-default">
                             <div class="panel-heading">
-                                <h3 class="panel-title"><strong>Arıza No #</strong> 3326</h3>
+                                <h3 class="panel-title"><strong>Arıza No #</strong> '.$_SESSION["id"].'</h3>
                             </div>
                             <div class="panel-body">
                                 <div class="table-responsive">
@@ -19,70 +20,33 @@
                                         <thead>
                                             <tr>
                                                 <th>Durum</th>
+                                                <th>Detay</th>
                                                 <th>İşlem Tarihi</th>
                                                 <th>Ücret</th>
                                             </tr>
                                         </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td class="col-xs-4">Onay Bekliyor</td>
-                                                <td class="col-xs-4">06/03/2016 , 13:29</td>
-                                                <td class="col-xs-4"><i class="fa fa-try"></i> 0</td>
-                                            </tr>
-                                            <tr>
-                                                <td class="col-xs-4">Onaylandı</td>
-                                                <td class="col-xs-4">06/03/2016 , 13:30</td>
-                                                <td class="col-xs-4"><i class="fa fa-try"></i> 0</td>
-                                            </tr>
-                                            <tr>
-                                                <td class="col-xs-4">Ekip yollandı</td>
-                                                <td class="col-xs-4">06/03/2016 , 13:31</td>
-                                                <td class="col-xs-4"><i class="fa fa-try"></i> 0</td>
-                                            </tr>
-                                            <tr>
-                                                <td class="col-xs-4">Telefon alındı</td>
-                                                <td class="col-xs-4">06/03/2016 , 13:32</td>
-                                                <td class="col-xs-4"><i class="fa fa-try"></i> 0</td>
-                                            </tr>
-                                            <tr>
-                                                <td class="col-xs-4">Teknik ekibe teslim edildi</td>
-                                                <td class="col-xs-4">06/03/2016 , 13:33</td>
-                                                <td class="col-xs-4"><i class="fa fa-try"></i> 0</td>
-                                            </tr>
-
-                                            <tr>
-                                                <td class="col-xs-4">Arıza tespit edildi ve fiyat verildi</td>
-                                                <td class="col-xs-4">06/03/2016 , 13:34</td>
-                                                <td class="col-xs-4"><i class="fa fa-try"></i> 100</td>
-                                            </tr>
-                                            <tr>
-                                                <td class="col-xs-4">Ücret onaylandı</td>
-                                                <td class="col-xs-4">06/03/2016 , 13:35</td>
-                                                <td class="col-xs-4"><i class="fa fa-try"></i> 0</td>
-                                            </tr>
-                                            <tr>
-                                                <td class="col-xs-4">Onarım aşamasında</td>
-                                                <td class="col-xs-4">06/03/2016 , 13:36</td>
-                                                <td class="col-xs-4"><i class="fa fa-try"></i> 0</td>
-                                            </tr>
-                                            <tr>
-                                                <td class="col-xs-4">Onarıldı</td>
-                                                <td class="col-xs-4">06/03/2016 , 13:37</td>
-                                                <td class="col-xs-4"><i class="fa fa-try"></i> 0</td>
-                                            </tr>
-                                            <tr>
-                                                <td class="col-xs-4">Kargolandı</td>
-                                                <td class="col-xs-4">06/03/2016 , 13:38</td>
-                                                <td class="col-xs-4"><i class="fa fa-try"></i> 0</td>
-                                            </tr>
-                                            <tr>
-                                                <td class="col-xs-4">Teslimat tamamlandı</td>
-                                                <td class="col-xs-4">06/03/2016 , 13:39</td>
-                                                <td class="col-xs-4"><i class="fa fa-try"></i> 0</td>
-                                            </tr>
+                                        <tbody>';
+                                            
+                                            $result = mysqli_query($connection, 
+                                            "CALL durum_gecmis_goster('".$_SESSION['id']."')") or die("Query fail: " . mysqli_error());
+                                            while ($row = mysqli_fetch_array($result)){
+                                                echo '
+                                                <tr>
+                                                    <td class="col-xs-2">'.$row[0].'</td>
+                                                    <td class="col-xs-4">'.$row[1].'</td>
+                                                    <td class="col-xs-4">'.$row[2].'</td>
+                                                    <td class="col-xs-2"><i class="fa fa-try">'.$row[3].'</i></td>
+                                                </tr>';
+                                            }
+                                            
+                                            include 'dbsettings.php';
+                                            $result = mysqli_query($connection, 
+                                            "CALL toplam_maliyet('".$_SESSION['id']."',@toplam)") or die("Query fail: " . mysqli_error());
+                                            $row = mysqli_fetch_array($result);
+                                            echo '                                          
                                             <tr>
                                                 <td colspan="2"><strong>Toplam Ücret:</strong></td>
-                                                <td colspan="1"><i class="fa fa-try"></i> 100</td>
+                                                <td colspan="1"><i class="fa fa-try"></i> '.$row[@toplam].'</td>
                                             </tr>
                                         </tbody>
                                     </table>
@@ -126,4 +90,6 @@
     </div>
 </div>
 </body>
-</html>
+</html>';
+        
+?>
